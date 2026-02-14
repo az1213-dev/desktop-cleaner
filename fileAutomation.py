ebd9a795398266041818907c497b71660730b58b
az1213-dev <azapata12131@gmail.com>
2026-02-14 15:08:23 -0500

    add imports and helper functions

--- fileAutomation.py
+++ fileAutomation.py
@@ -1,3 +1,7 @@
+import os 
+from os import scandir
+from os.path import splitext, exists, join
+
 # CONFIGURATION
 
 SOURCE_DIR = "/Users/azapa/Desktop" 
@@ -23,3 +27,34 @@ AUDIO_EXTENSIONS = [
 DOC_EXTENSIONS = [
     ".txt", ".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx"
 ]   
+
+# HELPER FUNCTIONS
+
+def ensure_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path,exist_ok=True) 
+
+def make_unique(dest_dir, name):
+    filename, extension = splitext(name)
+    count = 1
+    new_name = name
+
+    while exists(join(dest_dir,new_name)):
+        new_name = filename + "_" + str(count) + extension
+        count += 1
+    
+    return new_name
+
+def get_dest(extension):
+    ext = extension.lower()
+
+    if ext in IMAGE_EXTENSIONS:
+        return DEST_DIR_IMAGES
+    if ext in VIDEO_EXTENSIONS:
+        return DEST_DIR_VIDEOS
+    if ext in AUDIO_EXTENSIONS:
+        return DEST_DIR_AUDIO
+    if ext in DOC_EXTENSIONS:
+        return DEST_DIR_DOCS
+    
+    return DEST_DIR_MISC
