8935ae7d6f76524fcf659115e8bf02158bca4076
az1213-dev <azapata12131@gmail.com>
2026-02-14 14:07:07 -0500

    add extension lists

---
+++ fileAutomation.py
@@ -0,0 +1,25 @@
+# CONFIGURATION
+
+SOURCE_DIR = "/Users/azapa/Desktop" 
+
+DEST_DIR_IMAGES = "/Users/azapa/Desktop/Images"
+DEST_DIR_VIDEOS = "/Users/azapa/Desktop/Images"
+DEST_DIR_AUDIO = "/Users/azapa/Desktop/Audio"
+DEST_DIR_DOCS = "/Users/azapa/Desktop/Documents"
+DEST_DIR_MISC = "/Users/azapa/Desktop/Misc"
+
+IMAGE_EXTENSIONS = [
+    ".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".bmp", ".heic"
+]
+
+VIDEO_EXTENSIONS = [
+    ".mp4", ".mov", ".avi", ".mkv", ".webm", ".flv"
+]
+
+AUDIO_EXTENSIONS = [
+    ".mp3", ".wav", ".aac", ".m4a", ".flac"
+]
+
+DOC_EXTENSIONS = [
+    ".txt", ".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx"
+]   
