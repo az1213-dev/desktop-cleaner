65a7e4e6115a2ea3e84231b17fcbf119c99b84dd
az1213-dev <azapata12131@gmail.com>
2026-02-15 09:55:26 -0500

    function complete (w/o moved_x vars)

--- downloads_cleaner.py
+++ downloads_cleaner.py
@@ -7,3 +7,45 @@ from fileAutomation import (
     scandir, splitext, join, move, 
     ensure_dir, make_unique, get_dest
 )
+
+def clean_downloads():
+    print("Scanning " + DOWNLOADS_DIR)
+
+    files_moved = 0
+    moved_imgs = 0
+    moved_videos = 0
+    moved_audio = 0
+    moved_docs = 0
+    moved_misc = 0
+
+   
+    with scandir(DOWNLOADS_DIR) as entries: 
+        for entry in entries: 
+            if entry.is_file():
+                _, ext = splitext(entry.name)
+                if not ext: 
+                    continue 
+
+                # Determine category of file
+                dest_dir = get_dest(ext)
+                
+                # Map folders to downloads folders
+                if dest_dir.endswith("Images"):
+                    dest_dir = DL_IMAGES
+                elif dest_dir.endswith("Videos"):
+                    dest_dir = DL_VIDEOS
+                elif dest_dir.endswith("Audio"):
+                    dest_dir = DL_AUDIO
+                elif dest_dir.endswith("Documents"):
+                    dest_dir = DL_DOCS
+                else: 
+                    dest_dir = DL_MISC
+
+                ensure_dir(dest_dir)
+
+                unique_name = make_unique(dest_dir,entry.name)
+                dest_path = join(dest_dir,unique_name)
+
+                print("Moving " + entry.name + " to " + dest_dir)
+                move(entry.path, dest_path)
+                files_moved += 1
