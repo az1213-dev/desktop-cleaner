697edc9971881dbc054b8dd8f362c3504549e51e
az1213-dev <azapata12131@gmail.com>
2026-02-15 09:45:59 -0500

    import functions and constants

---
+++ downloads_cleaner.py
@@ -0,0 +1,9 @@
+# IMPORTS
+from fileAutomation import (
+    # Constants
+    DOWNLOADS_DIR,
+    DL_IMAGES, DL_AUDIO, DL_DOCS, DL_MISC, DL_VIDEOS,
+    # Functions
+    scandir, splitext, join, move, 
+    ensure_dir, make_unique, get_dest
+)
