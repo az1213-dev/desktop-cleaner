2402cf09684806e7f64a0e7e358ce2fa7c36f56f
az1213-dev <azapata12131@gmail.com>
2026-08-13 12:52:20 -0400

    removed old comments

--- config.py
+++ config.py
@@ -3,17 +3,7 @@ import json
 
 HOME = os.path.expanduser("~")
 
-# SOURCE DIRECTORIES
-# Note: there's no fixed "desktop" source anymore. The drive to organize
-# is chosen interactively at runtime (see helpers.get_available_drives
-# and main.select_drive). DOWNLOADS_DIR stays fixed since it's almost
-# always the same known location.
-
 DOWNLOADS_DIR = os.path.join(HOME, "Downloads")
-
-# CATEGORY -> EXTENSIONS MAP
-# Loaded from categories.json instead of being hardcoded here.
-# Add new extensions by editing that file, not this one.
 
 CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
 CATEGORIES_FILE = os.path.join(CONFIG_DIR, "categories.json")
@@ -39,5 +29,4 @@ def load_categories():
 
 CATEGORY_EXTENSIONS, MISC_CATEGORY = load_categories()
 
-# Fixed order used for printing summaries so output is consistent
 CATEGORY_ORDER = list(CATEGORY_EXTENSIONS.keys()) + [MISC_CATEGORY]
